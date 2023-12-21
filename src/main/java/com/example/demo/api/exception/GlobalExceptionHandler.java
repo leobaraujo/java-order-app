86a9b7e95af2cc026c1a29a5eb9ca1343247c416
leandroaraujowm <leandro.araujowm@gmail.com>
2023-12-21 17:09:31 -0300

    fix: add new exceptions handlers

--- src/main/java/com/example/demo/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/api/exception/GlobalExceptionHandler.java
@@ -4,21 +4,42 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.api.dto.GlobalExceptionDTO;
 
+import jakarta.validation.ConstraintViolationException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Invalid path variable
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<GlobalExceptionDTO> illegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GlobalExceptionDTO(
+        return badRequestResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    // Missing request body
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<GlobalExceptionDTO> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return badRequestResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    // Invalid request body
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<GlobalExceptionDTO> constraintViolationException(ConstraintViolationException e) {
+        return badRequestResponse(HttpStatus.BAD_GATEWAY, e.getMessage());
+    }
+
+    private ResponseEntity<GlobalExceptionDTO> badRequestResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status).body(new GlobalExceptionDTO(
                 Instant.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
-                e.getMessage()));
+                status.value(),
+                status.name(),
+                message));
+
     }
 
 }
