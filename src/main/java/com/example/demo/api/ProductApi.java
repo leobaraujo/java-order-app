2e5d326f87310d2af6702566af7ce4c9474f9f36
leandroaraujowm <leandro.araujowm@gmail.com>
2023-12-21 17:09:03 -0300

    feat: add Product createProduct endpoint

--- src/main/java/com/example/demo/api/ProductApi.java
+++ src/main/java/com/example/demo/api/ProductApi.java
@@ -1,5 +1,6 @@
 package com.example.demo.api;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,9 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.demo.api.dto.NewProductDTO;
 import com.example.demo.domain.entity.Product;
 import com.example.demo.service.ProductService;
 
@@ -39,6 +44,15 @@ public class ProductApi {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createProduct(@RequestBody NewProductDTO newProductDTO) {
+        Product createdProduct = productService.createProduct(newProductDTO);
+        URI resourceLocation = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(createdProduct.getId()).toUri();
+
+        return ResponseEntity.created(resourceLocation).build();
     }
 
 }
