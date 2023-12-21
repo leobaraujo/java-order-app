bc14b907d0451992f875489d425d50a3bc8dc516
leandroaraujowm <leandro.araujowm@gmail.com>
2023-12-21 15:34:14 -0300

    feat: add Product getById endpoint

--- src/main/java/com/example/demo/api/ProductApi.java
+++ src/main/java/com/example/demo/api/ProductApi.java
@@ -1,10 +1,12 @@
 package com.example.demo.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,17 @@ public class ProductApi {
         List<Product> productList = productService.getAll();
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable UUID id) {
+        try {
+            Product product = productService.getById(id);
+
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
