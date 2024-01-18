eb21ce54a3665942344192b9922d22ceec748d29
leandroaraujowm <leandro.araujowm@gmail.com>
2024-01-18 12:22:43 -0300

    feat: add Order deleteOrderById endpoint

--- src/main/java/com/example/demo/api/OrderApi.java
+++ src/main/java/com/example/demo/api/OrderApi.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,13 @@ public class OrderApi {
         orderService.updateOrder(id, updateOrderDTO);
 
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderById(@PathVariable UUID id) {
+        orderService.deleteOrderById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
