2eaeffcab16b19be49f08fc65feaba7dee1eadf5
leandroaraujowm <leandro.araujowm@gmail.com>
2024-01-30 15:26:06 -0300

    feat: add User entity and userRoles

---
+++ src/main/java/com/example/demo/domain/entity/User.java
@@ -0,0 +1,102 @@
+package com.example.demo.domain.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user_table")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Size(min = 4, max = 32)
+    private String username;
+
+    @NotBlank
+    @Size(min = 4, max = 32)
+    private String password;
+
+    @NotNull
+    private String role;
+
+    public User() {
+    }
+
+    public User(UUID id, @NotBlank @Size(min = 4, max = 32) String username,
+            @NotBlank @Size(min = 4, max = 32) String password, @NotNull String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/demo/domain/entity/UserRoles.java
@@ -0,0 +1,6 @@
+package com.example.demo.domain.entity;
+
+public enum UserRoles {
+    ADMIN,
+    WORKER
+}
